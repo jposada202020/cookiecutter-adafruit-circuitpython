6d453bb25abd010f48624a47d4e352191310a10d
jposada202020 <jquintana202020@gmail.com>
2023-04-29 19:18:42 -0400

    cp

--- {{ cookiecutter.__dirname }}/{{ cookiecutter.__libprefix }}{{ cookiecutter.__libname }}.py
+++ {{ cookiecutter.__dirname }}/{{ cookiecutter.__libprefix }}{{ cookiecutter.__libname }}.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) {% now 'utc', '%Y' %} {{ cookiecutter.author_name }}
+# SPDX-FileCopyrightText: Copyright (c) {% now 'utc', '%Y' %} Jose D. Montoya
 #
 # SPDX-License-Identifier: MIT
 """
@@ -18,3 +18,18 @@
 
 # imports
 
+{% if cookiecutter.target_bundle != 'CircuitPython Org' -%}
+    {%- if cookiecutter.library_prefix -%}
+        {%- set repo_name = (cookiecutter.library_prefix | capitalize) -%}
+        {%- set repo_name = repo_name + '_CircuitPython_' -%}
+        {%- set repo_name = repo_name + cookiecutter.library_name | replace(" ", "_") -%}
+    {%- else -%}
+        {%- set repo_name = 'CircuitPython_' -%}
+        {%- set repo_name = repo_name + cookiecutter.library_name | replace(" ", "_") -%}
+    {%- endif -%}
+{% else -%}
+    {%- set repo_name = 'CircuitPython_Org_' + cookiecutter.library_name | replace(" ", "_") -%}
+{% endif -%}
+
+__version__ = "0.0.0+auto.0"
+__repo__ = "https://github.com/{{ cookiecutter.github_user }}/{{ repo_name }}.git"
